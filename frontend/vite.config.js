9bd79dc0170411255644ae54a73f3eb364756f09
Sourav <dalmotrasourav2003@gmail.com>
2026-05-26 21:34:10 +0530

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,33 +1,10 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// export default defineConfig({
-//   plugins: [
-//     react(),
-//     tailwindcss(),
-//   ],
-// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // Splits third-party dependencies from node_modules into a separate 'vendor' chunk
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-      },
-    },
-  },
 })
